5768c850923a4d7ff6e61206e2b8b3fce10716da
salraffone-sudo <salraffone@gmail.com>
2026-09-02 12:36:18 -0400

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>MOONapp</title>
+      </Head>
       <Component {...pageProps} />
     </>
   );
